605db721810a1a6f0b5af111351eca17631d0853
Tim <g.phh@mail.ru>
2022-10-27 21:44:44 +0300

    Initial commit.

---
+++ smartQSort/smartQSort.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+int getSeparator(int* arrayToSort, int leftElementNumber, int rightElementNumber) {
+
+}
+
+void partQSort(int* arrayToSort, int leftElementNumber, int rightElementNumber) {
+    if (leftElementNumber < rightElementNumber) {
+        int separator = getSeparator(arrayToSort, leftElementNumber, rightElementNumber);
+        partQSort(arrayToSort, leftElementNumber, separator);
+        partQSort(arrayToSort, separator + 1, rightElementNumber);
+    }
+}
+
+bool test(void) {
+
+}
+
+int main(void) {
+
+
+    return 0;
+}
