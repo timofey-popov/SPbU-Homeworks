9463bd7af12c25e2a275dac390027c2845c266bc
Tim <g.phh@mail.ru>
2022-11-08 19:41:37 +0300

    Add comments and tests.

--- search/search.c
+++ search/search.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <stdlib.h>
 
+// Функция для обработки пользовательского ввода (целые числа).
+// На вход принимает указатель на переменную, в которую нужно записать значение.
 void input(int* value) {
     int correctnessCheck = scanf_s("%d", value);
 
@@ -17,9 +19,8 @@ void input(int* value) {
 }
 
 // Сгенерировать массив размера wantedSize.
-// Требует подключения time.h и stdlib.h
+// На вход принимает указатель на пустой массив и желаемое количество элементов.
 void arrayGenerate(int* arrayToGen, int wantedSize) {
-    srand((unsigned)time(NULL));
     for (int i = 0; i < wantedSize; ++i)
         arrayToGen[i] = (rand() % 1000);
 }
@@ -27,10 +28,14 @@ void arrayGenerate(int* arrayToGen, int wantedSize) {
 // Функция для вывода массива на экран.
 // Принимает указатель на массив, размер массива и количество элементов в одной строке вывода.
 void arrayPrint(int* arrayToPrint, int printingArraySize, int numberOfElementsInOneString) {
-    for (int i = 1; i < printingArraySize + 1; ++i) {
-        printf("%7d", arrayToPrint[i - 1]);
-        if (i % numberOfElementsInOneString == 0)
-            printf("\n");
+    int stringsCounter = 1;
+
+    for (int i = 0; i < printingArraySize; ++i) {
+        if (i % (numberOfElementsInOneString + 1) == 0) {
+            printf("\n%4d |", stringsCounter);
+            stringsCounter++;
+        }
+        printf("%7d", arrayToPrint[i]);
     }
 
     printf("\n\n");
@@ -38,21 +43,25 @@ void arrayPrint(int* arrayToPrint, int printingArraySize, int numberOfElementsIn
 
 // Функция бинарного поиска числа в массиве.
 // На вход принимает указатель на массив, искомое число и левый и правый индексы части массива, в которой ищем.
+// Возвращает true, если нашла искомое число в данном массиве, и false в противном случае.
 // Корректно работает только на отсортированном массиве.
 bool search(int* array, int value, int left, int right) {
     int arrayLength = right - left + 1;
 
     if (arrayLength < 3) {
-        if (array[left] == value || array[right] == value)
+        if (array[left] == value || array[right] == value) {
             return true;
+        }
+
         return false;
     }
 
     int middleIndex = left + (arrayLength / 2);
     int middleValue = array[middleIndex];
 
-    if (value = middleValue)
+    if (value == middleValue) {
         return true;
+    }
     if (value < middleValue)
         return search(array, value, left, middleIndex - 1);
     if (value > middleValue)
@@ -61,6 +70,8 @@ bool search(int* array, int value, int left, int right) {
     return false;
 }
 
+// Функция быстрой сортировки.
+// На вход принимает указатель на сортируемый массив и левый и правый индексы сортируемого куска массива.
 void quickSort(int* array, int leftIndex, int rightIndex) {
     if (leftIndex < rightIndex) {
         int left = leftIndex;
@@ -83,19 +94,96 @@ void quickSort(int* array, int leftIndex, int rightIndex) {
         array[rightIndex] = array[left];
         array[left] = temporary;
 
-        printf("Calling qsort with %d and %d AND %d and %d\n", leftIndex, left - 1, left + 1, rightIndex);
-        quickSort(array, leftIndex, left - 1);
-        quickSort(array, left + 1, rightIndex);
+        if (leftIndex < left - 1)
+            quickSort(array, leftIndex, left - 1);
+        if (left + 1 < rightIndex)
+            quickSort(array, left + 1, rightIndex);
     }
 
     return;
 }
 
-bool test(void) {
+// Далее две функции тестов, отдельно для функции сортировки и поиска.
+// Если обе функции работают корректно, то их "композиция" тоже будте работать корректно.
+bool testForSort(void) {
+    bool test1 = true;
+    int testArray1[] = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+    int referenceArray1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 
+    quickSort(testArray1, 0, 10);
+
+    for (int i = 0; i < 11; ++i) {
+        if (testArray1[i] != referenceArray1[i]) {
+            test1 = false;
+        }
+    }
+
+    bool test2 = true;
+    int testArray2[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+    int referenceArray2[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+    quickSort(testArray2, 0, 10);
+
+    for (int i = 0; i < 11; ++i) {
+        if (testArray2[i] != referenceArray2[i]) {
+            test2 = false;
+        }
+    }
+
+    bool test3 = true;
+    int testArray3[] = { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int referenceArray3[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+
+    quickSort(testArray3, 0, 10);
+
+    for (int i = 0; i < 11; ++i) {
+        if (testArray3[i] != referenceArray3[i]) {
+            test3 = false;
+        }
+    }
+
+    if (test1 && test2 && test3) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool testForSearch(void) {
+    bool test1 = true;
+    int testArray1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+    if (!search(testArray1, 11, 2, 10)) {
+        test1 = false;
+    }
+
+    bool test2 = true;
+    int testArray2[] = { 0 };
+
+    if (!search(testArray1, 0, 0, 0)) {
+        test1 = false;
+    }
+
+    if (test1 && test2) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 int main(void) {
+    if (testForSort() && testForSearch) {
+        printf("*Tests passed*\n\n");
+    }
+    else {
+        printf(testForSort() ? "Sort test passed\n" : "Sort test failed\n");
+        printf(testForSearch() ? "Search test passed\n" : "Search test failed\n");
+    }
+
+    srand((unsigned)time(NULL));
+
     int n = 0;
     int k = 0;
       
@@ -126,17 +214,17 @@ int main(void) {
     printf("Second array (k)\n");
     arrayPrint(numbersForSearch, k, 10);
 
-    /*for (int i = 0; i < k; ++i) {
-        if (search(randomArray, numbersForSearch[i], 0, k - 1))
-            printf("There is number %d in the random array\n", numbersForSearch[i]);
-        else
-            printf("There is NO number %d in the random array\n", numbersForSearch[i]);
-    }*/
-
     quickSort(randomArray, 0, n - 1);
 
     printf("Sorted first array (n):\n");
     arrayPrint(randomArray, n, 10);
+
+    for (int i = 0; i < k; ++i) {
+        if (search(randomArray, numbersForSearch[i], 0, n - 1))
+            printf("There is number %d in the random array\n", numbersForSearch[i]);
+        else
+            printf("There is NO number %d in the random array\n", numbersForSearch[i]);
+    }
 
     return 0;
 }
