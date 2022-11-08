d95a87ea8ea8d9c8c33a1918c74c27019b324dcd
Tim <g.phh@mail.ru>
2022-11-08 22:02:55 +0300

    Add some comments, place functions by order.

--- phoneBook/phoneBook.c
+++ phoneBook/phoneBook.c
@@ -2,11 +2,14 @@
 #include <string.h>
 #include <stdbool.h>
 
+// Структура для записей телефонной книги.
 typedef struct {
     char name[51];
     char phone[51];
 } PhonebookStruct;
 
+// Вспомогательная функция. Полностью очищает поток ввода.
+// Нужно использовать аккуратно, так как если в потоке ничего нет, функция будет ждать следующего ввода и очистит этот ввод.
 void customFlush(void) {
     scanf("%*[^\n]");
     getchar();
@@ -14,6 +17,8 @@ void customFlush(void) {
     return;
 }
 
+// Функция для обработки пользовательского ввода (строк).
+// На вход принимает массив для записи и размер этого массива.
 void stringsInput(char* arrayForInput, int arraySize) {
     char* isStringInputCorrect = fgets(arrayForInput, arraySize, stdin);
     while (!isStringInputCorrect) {
@@ -25,9 +30,47 @@ void stringsInput(char* arrayForInput, int arraySize) {
     return;
 }
 
+// Функция для начального считывания данных из файла.
+// Считывает количество записей и сами записи в соответствующие переменные.
+// Если файла не существует, он будет создан.
+void initialFileReading(PhonebookStruct* book, int* counterOfRecords) {
+    FILE* file = fopen("phonebook.txt", "a+");
+    int fileEmptinessChecker = fscanf(file, "%d", counterOfRecords);
+
+    if (fileEmptinessChecker > 0) {
+        fgetc(file);
+        for (int i = 0; i < *counterOfRecords; ++i) {
+            fgets(book[i].name, 51, file);
+            fgets(book[i].phone, 51, file);
+        }
+    }
+
+    fclose(file);
+    return;
+}
+
+// Функция получения номера действия от пользователя.
+// Записывает введённый номер действия в специальную переменную.
+void numberOfActionInput(short* statusForChange) {
+
+    printf("Choose an action:\n0 - exit\n1 - make a record\n"
+        "2 - show all records\n3 - find a number by name\n4 - find a name by number\n5 - save data to file\n\n");
+
+    int isInputCorrect = scanf("%hi", statusForChange);
+    while (!isInputCorrect || *statusForChange < 0 || *statusForChange > 5) {
+        scanf("%*[^\n]");
+        printf("Wrong input. Try again:\n");
+        isInputCorrect = scanf("%hi", statusForChange);
+    }
+
+    return;
+}
+
 // Функция, которая создаёт запись в массиве структур и прибавляет счётчик после каждой созданной записи.
 // На вход принимает указатель на счётчик и на массив структур.
 void newRecordCreate(int* counterOfRecords, PhonebookStruct* arrayForRecord) {
+
+    //Очищаем поток, так как в нём уже находится символ перевода строки после введённого пользователем номера действия.
     customFlush();
 
     char nameWithinFunction[51];
@@ -92,7 +135,7 @@ void search(PhonebookStruct* book, char* stringForCompare, int counterOfRecords,
     return;
 }
 
-void phoneByName(PhonebookStruct* book, int counterOfRecords) {
+void findPhoneByName(PhonebookStruct* book, int counterOfRecords) {
     customFlush();
 
     char name[51];
@@ -105,7 +148,7 @@ void phoneByName(PhonebookStruct* book, int counterOfRecords) {
     return;
 }
 
-void nameByPhone(PhonebookStruct* book, int counterOfRecords) {
+void findNameByPhone(PhonebookStruct* book, int counterOfRecords) {
     customFlush();
 
     char phone[51];
@@ -131,35 +174,6 @@ void recordToFile(PhonebookStruct* book, int counterOfRecords) {
     printf("\nData saved successfully.\n\n");
 }
 
-void initialFileReading(PhonebookStruct* book, int* counterOfRecords) {
-    FILE* file = fopen("phonebook.txt", "a+");
-    int fileEmptynessChecker = fscanf(file, "%d", counterOfRecords);
-
-    if (fileEmptynessChecker > 0) {
-        fgetc(file);
-        for (int i = 0; i < *counterOfRecords; ++i) {
-            fgets(book[i].name, 51, file);
-            fgets(book[i].phone, 51, file);
-        }
-    }
-
-    fclose(file);
-}
-
-// Функция получения номера действия от пользователя.
-void numberOfActionInput(short* statusForChange) {
-
-    printf("Choose an action:\n0 - exit\n1 - make a record\n"
-        "2 - show all records\n3 - find a number by name\n4 - find a name by number\n5 - save data to file\n\n");
-
-    int isInputCorrect = scanf("%hi", statusForChange);
-    while (!isInputCorrect || *statusForChange < 0 || *statusForChange > 5) {
-        scanf("%*[^\n]");
-        printf("Wrong input. Try again:\n");
-        isInputCorrect = scanf("%hi", statusForChange);
-    }
-}
-
 int main(void) {
     PhonebookStruct phonebookArray[100];
 
@@ -167,7 +181,7 @@ int main(void) {
     short currentStatus = 1;
 
     // Переменная для хранения количества записей.
-    // Считывается из файла при запуске программы, увеличивается с каждой следующей записью. 
+    // Считывается из файла при запуске программы, увеличивается с каждой следующей записью.
     int counterOfRecords = 0;
 
     initialFileReading(phonebookArray, &counterOfRecords);
@@ -186,20 +200,14 @@ int main(void) {
             printWholeBook(phonebookArray, counterOfRecords);
             break;
         case 3:
-            phoneByName(phonebookArray, counterOfRecords);
+            findPhoneByName(phonebookArray, counterOfRecords);
             break;
         case 4:
-            nameByPhone(phonebookArray, counterOfRecords);
+            findNameByPhone(phonebookArray, counterOfRecords);
             break;
         case 5:
             recordToFile(phonebookArray, counterOfRecords);
             break;
         }
     }
-
-    for (int i = 0; i < counterOfRecords; ++i) {
-        printf("\nName: %sPhone: %s\n", phonebookArray[i].name, phonebookArray[i].phone);
-    }
 }
-
-// Вопросы:
