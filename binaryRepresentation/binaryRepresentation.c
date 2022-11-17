c3790ff3090ed039322945259d0e2dfbada990d8
Tim <g.phh@mail.ru>
2022-11-17 18:37:40 +0300

    Add binary numbers generating function.

--- binaryRepresentation/binaryRepresentation.c
+++ binaryRepresentation/binaryRepresentation.c
@@ -1,39 +1,84 @@
 ﻿#include <stdio.h>
 #include <locale.h>
 
-// Функция генерации двоичного представленмя числа, перевёрнутого задом наперёд.
-// Потом этот массив будет перевёрнут и число будет каким и должно быть.
+#define NUMBER_OF_DIGITS 8
 
-//void binGenerator(int decimal, int* arrayForBinary)
-//{
-//    int decimalCopy = decimal;
-//
-//    int i = 0;
-//    while (decimalCopy)
-//    {
-//        arrayForBinary[i] = decimalCopy % 2;
-//        decimalCopy = decimalCopy / 2;
-//        ++i;
-//    }
-//
-//}
+//Функция генерации двоичного представленмя числа, записанного cлева направо в массиве.
+void generateBinary(int decimal, int* arrayForBinary) {
+    int decimalCopy = decimal;
+    int buffer[NUMBER_OF_DIGITS] = { 0 };
 
-void arrayPrint(int* arrayToPrint, int printingArraySize)
-{
-    for (int i = 1; i < printingArraySize + 1; ++i)
-    {
+    for (int i = NUMBER_OF_DIGITS; i >= 0; --i) {
+        buffer[i] = decimalCopy % 2;
+        decimalCopy = decimalCopy / 2;
+    }
+
+    if (decimal >= 0) {
+        for (int i = 0; i < NUMBER_OF_DIGITS; ++i) {
+            arrayForBinary[i] = buffer[i];
+        }
+    } else {
+        int bufferForAdditionalCode[NUMBER_OF_DIGITS] = { 0 };
+        createAdditionalCode(buffer, bufferForAdditionalCode)
+    }
+}
+
+int carryOut(int firstNumber, int secondNumber) {
+    return firstNumber && secondNumber;
+}
+
+void addBinary(int* firstSummand, int* secondSummand, int* binaryResult) {
+    int carryOutValue = 0;
+
+    for (int i = NUMBER_OF_DIGITS - 1; i >= 0; --i) {
+        binaryResult [i] = firstSummand[i] ^ secondSummand[i] ^ carryOutValue;
+
+        
+
+    }
+}
+
+void createAdditionalCode(int* binarySubtrahend, int* binaryResult) {
+    int binaryMinuend[] = { 1, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int carryOutValue = 0;
+
+    for (int i = NUMBER_OF_DIGITS - 1; i >= 0; --i) {
+        binaryResult[i] = ((binaryMinuend[i + 1] ^ binarySubtrahend[i]) ^ carryOutValue);
+
+
+        switch (carryOutValue) {
+        case 0:
+            carryOutValue = !binaryMinuend[i + 1] && binarySubtrahend[i];
+            break;
+        case 1:
+            carryOutValue = !binaryMinuend[i + 1] || binarySubtrahend[i];
+            break;
+        }
+    }
+}
+
+void printBinary(int* numberToPrint) {
+    for (int i = 0; i < NUMBER_OF_DIGITS; ++i) {
+        printf("%d", numberToPrint[i]);
+    }
+
+    printf("\n");
+}
+
+void printArray(int* arrayToPrint, int printingArraySize) {
+    for (int i = 1; i < printingArraySize + 1; ++i) {
         printf("%7d", arrayToPrint[i - 1]);
-        if (i % 10 == 0)
-        {
+        if (i % 10 == 0) {
             printf("\n");
         }
     }
+
     printf("\n\n");
 }
 
-void integerUserInput(int* valueToWrite, int leftLimit, int rightLimit) {
+void inputInteger(int* valueToWrite, int leftLimit, int rightLimit) {
     int isInputCorrect = scanf("%d", valueToWrite);
-    printf("%d\n", isInputCorrect);
+
     while (!isInputCorrect || *valueToWrite > rightLimit || *valueToWrite < leftLimit) {
         printf("Некорректный ввод, попробуйте ещё раз:\n");
         scanf("%*[^\n]");
@@ -49,17 +94,22 @@ int main(void)
     int firstNumber = 0;
     int secondNumber = 0;
 
-    int binFirstNumber[8] = { 0 };
-    int binSecondNumber[8] = { 0 };
+    int binFirstNumber[NUMBER_OF_DIGITS] = { 0 };
+    int binSecondNumber[NUMBER_OF_DIGITS] = { 0 };
 
     printf("Введите первое число (от -128 до 128):\n");
-    integerUserInput(&firstNumber, -128, 128);
+    inputInteger(&firstNumber, -128, 128);
 
     printf("Введите второе число (от -127 до 127):\n");
-    integerUserInput(&secondNumber, -127, 127);
+    inputInteger(&secondNumber, -127, 127);
 
-    printf("%d\n", firstNumber);
-    printf("%d\n", secondNumber);
+    printf("Первое введённое число: %d\n", firstNumber);
+    printf("Второе введённое число: %d\n", secondNumber);
+
+    generateBinary(firstNumber, binFirstNumber);
+
+    printf("Двоичное представление %d это ", firstNumber);
+    printBinary(binFirstNumber);
 
     return 0;
 }
