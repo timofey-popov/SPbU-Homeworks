95b327d288d0a28946e75caccdcea4c99eee6bda
Tim <g.phh@mail.ru>
2022-11-18 21:59:50 +0300

    Add test module.

---
+++ binaryRepresentation/tests.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "binaryRepresentation.h"
+
+
+
+bool testForPositiveExponent(void) {
+    return positiveExponentOfTwo(8) == 256 && positiveExponentOfTwo(0) == 1;
+}
+
+bool testForCreateAdditionalCode(void) {
+
+}
