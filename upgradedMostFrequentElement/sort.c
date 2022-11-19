23c0a5d5000c859fa9a846b633d91025cdae5297
Tim <g.phh@mail.ru>
2022-11-19 18:10:02 +0300

    Initial commit.

---
+++ upgradedMostFrequentElement/sort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Функция разделения массива на две части.
+// Слева элементы меньшие либо равные последнему элементу массива, справа - бОльшие последнего.
+// В конце выполнения помещает последний элемент (разделитель) между этими двумя частями массива.
+// Возвращает индекс этого элемента-разделителя.
+int partition(int* arrayToSort, int leftElementNumber, int rightElementNumber) {
+    int pivot = arrayToSort[rightElementNumber];
+
+    int i = leftElementNumber;
+    for (int j = leftElementNumber; j < rightElementNumber; ++j) {
+        if (arrayToSort[j] <= pivot) {
+            int temporary = arrayToSort[i];
+            arrayToSort[i] = arrayToSort[j];
+            arrayToSort[j] = temporary;
+
+            ++i;
+        }
+    }
+
+    int temporary = arrayToSort[i];
+    arrayToSort[i] = arrayToSort[rightElementNumber];
+    arrayToSort[rightElementNumber] = temporary;
+
+    return i;
+}
+
+// Рекурсивная функция быстрой сортировки.
+// Вызывает себя от начала до индекса, возвращаемого функцией разделения, - 1, и от индекса + 1 до конца массива.
+// Сам элемент с индексом, возвращённым функцией разделения, оставляет на месте.
+// Если элементов в массиве меньше 10, функция сортирует их обычными вставками.
+void quickSort(int* arrayToSort, int leftElementNumber, int rightElementNumber) {
+    if ((rightElementNumber - leftElementNumber + 1) < 10) {
+        for (int i = leftElementNumber + 1; i < rightElementNumber + 1; ++i) {
+            int k = i;
+            while (k > leftElementNumber && arrayToSort[k] < arrayToSort[k - 1]) {
+                int temporary = arrayToSort[k];
+                arrayToSort[k] = arrayToSort[k - 1];
+                arrayToSort[k - 1] = temporary;
+
+                k--;
+            }
+        }
+    }
+
+    else if (leftElementNumber < rightElementNumber) {
+        int separator = partition(arrayToSort, leftElementNumber, rightElementNumber);
+        quickSort(arrayToSort, leftElementNumber, separator - 1);
+        quickSort(arrayToSort, separator + 1, rightElementNumber);
+    }
+}
---
+++ upgradedMostFrequentElement/upgradedMostFrequentElement.c
@@ -0,0 +1,54 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include "sort.h"
+
+// Функция, считающая самый частый элемент в массиве.
+// На вход принимает ссылку на массив и его размер, возвращает значение самого частого элемента.
+int mostFrequentElement(int* arrayToOperate, size_t arraySize) {
+    quickSort(arrayToOperate, 0, arraySize - 1);
+
+    int counter = 1;
+    int maximumCounter = 1;
+    int frequentElement = arrayToOperate[0];
+
+    for (size_t i = 1; i < arraySize; ++i) {
+        if (arrayToOperate[i] == arrayToOperate[i - 1]) {
+            counter++;
+        }
+        else {
+            counter = 1;
+        }
+
+        if (counter > maximumCounter) {
+            maximumCounter = counter;
+            frequentElement = arrayToOperate[i];
+        }
+    }
+
+    return frequentElement;
+}
+
+// Функция-тест
+bool test(void) {
+    int testArray1[] = { 1, 2, 3, 4, 4, 5 };
+    int testArray2[] = { 0, 0, 0, 0, 0, 0 };
+    int testArray3[] = { 1, 2, 3, 4, 5, 6 };
+
+    return (mostFrequentElement(testArray1, 6) == 4 && mostFrequentElement(testArray2, 6) == 0 && mostFrequentElement(testArray3, 6) == 1);
+}
+
+int main(void) {
+    if (!test()) {
+        printf("Test failed.\n");
+        return -1;
+    } else {
+        printf("*test passed*\n\n");
+    }
+
+    int arrayToOperate[] = { 1, 5, 5, 6, 2, 3, 4, 7, 5, 1, 2, 3, 1, 1 };
+    size_t sizeOfArray = sizeof(arrayToOperate) / sizeof(int);
+
+    printf("Most frequent element = %d\n", mostFrequentElement(arrayToOperate, sizeOfArray));
+
+    return 0;
+}
