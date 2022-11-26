c258d18927231ee3c29ac82a3957df847f2327c3
Tim <g.phh@mail.ru>
2022-11-26 15:14:36 +0300

    Change stack structure according to ADT conception.

--- customStack/customStack.c
+++ customStack/customStack.c
@@ -1,98 +1,135 @@
-﻿#include "customStack.h"
-#include <stdlib.h>
+﻿#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-
-// Все фунции в случае ошибок меняют переменную errorCode с 0 на 1.
+#include "customStack.h"
 
 // Структура для единицы стека.
 // Содержит два поля: значение и указатель на предыдущую единицу стека.
-typedef struct Unit {
-    int value;
-    struct Unit* previous;
-} Unit;
+typedef struct StackElement {
+    Value value;
+    struct StackElement* previous;
+} StackElement;
 
-// Функция для добавления элемента стека.
-// На вход принимает значение, которое нужно добавить, указатель на голову стека и указатель на переменную с кодом ошибки.
-void push(int value, Unit** head, int* errorCode) {
-    Unit* newHead = malloc(sizeof(Unit));
+// Структукра "стек". Представляет собой указатель на глову стека.
+typedef struct Stack {
+    StackElement* head;
+} Stack;
 
-    if (newHead == NULL) {
-        *errorCode = 1;
-        printf("Memory allocation problem\n");
+
+// Создаёт пустой стек.
+// На вход принимает указатель на переменную с кодом ошибки.
+// Возвращает указатель на стек, если всё прошло нормально,в противном случае возвращает NULL и меняет код ошибки на 1.
+Stack* createStack(ErrorCodes* errorCode) {
+    Stack* newStack = malloc(sizeof(Stack));
+
+    if (newStack == NULL) {
+        *errorCode = createStackMemoryError;
+        return NULL;
+    }
+
+    newStack->head = NULL;
+
+    return newStack;
+}
+
+// Добавляет элемент на верхушку стека.
+// На вход принимает добавляемое значение, указатель на стек и указатель на переменную с кодом ошибки.
+void push(Value value, Stack* stack, ErrorCodes* errorCode) {
+    if (stack == NULL) {
+        *errorCode = pushGotNullPointer;
         return;
     }
 
-    newHead->previous = *head;
-    newHead->value = value;
+    StackElement* newHead = malloc(sizeof(StackElement));
 
-    *head = newHead;
-}
-
-// Функция для удаления элемента из стека, которая возвращает значение этого элемента.
-// На вход принимает указатель на голову стека и указатель на переменную с кодом ошибки.
-int pop(Unit** head, int* errorCode) {
-    if (*head == NULL) {
-        *errorCode = 1;
-        printf("Null element pop attempt\n");
-        return -1;
+    if (newHead == NULL) {
+        *errorCode = pushMemoryError;
+        return;
     }
 
-    int valueToReturn = (*head)->value;
-    Unit* temporaryAddress = (*head)->previous;
+    newHead->previous = stack->head;
+    newHead->value = value;
 
-    free(*head);
-    *head = temporaryAddress;
+    stack->head = newHead;
+}
+
+// Удаляет элемент с вершины стека и возвращает значение, которое в нём лежало.
+// На вход принимает указатель на стек и указатель на переменную с кодом ошибки.
+Value pop(Stack* stack, ErrorCodes* errorCode) {
+    if (stack == NULL) {
+        *errorCode = popGotNullPointer;
+        return NULL;
+    }
+
+    if (stack->head == NULL) {
+        *errorCode = popStackHasNoElemens;
+        return NULL;
+    }
+
+    Value valueToReturn = stack->head->value;
+    StackElement* temporaryAddress = stack->head->previous;
+
+    free(stack->head);
+    stack->head = temporaryAddress;
 
     return valueToReturn;
 }
 
-// Функция полной очистки стека.
-// На вход принимает указатель на голову стека.
-void clear(Unit** head) {
-    while (*head != NULL) {
-        pop(head, NULL);
+// Удаляет все элементы из стека, никуда их не сохраняя.
+// На вход принимает указатель на стек и указатель на переменную с кодом ошибки.
+void clear(Stack* stack, ErrorCodes* errorCode) {
+    if (stack == NULL) {
+        *errorCode = clearGotNullPointer;
+        return;
+    }
+
+    while (stack->head != NULL) {
+        pop(stack, errorCode);
     }
 }
 
 // Функция проверки стека на пустоту.
 // На вход принимает указатель на голову стека.
 // Возвращает true, если стек пуст, и false в противном случае.
-bool isEmpty(Unit** head) {
-    return *head == NULL;
+bool isEmpty(Stack* stack, ErrorCodes* errorCode) {
+    if (stack == NULL) {
+        *errorCode = isEmptyGotNullPointer;
+        return NULL;
+    }
+
+    return stack->head == NULL;
 }
 
-// Функция, печатающая стек на экран (никак не меняет стек).
-// На вход принимает указатель на голову стека.
-void printStack(Unit** head) {
-    Unit* headCopy = *head;
-
-    if (headCopy == NULL) {
-        printf("There are no elements in stack\n\n");
+// Удалить указанный стек.
+void deleteStack(Stack* stack, ErrorCodes* errorCode) {
+    if (stack == NULL) {
+        *errorCode = deleteStackGotNullPointer;
         return;
     }
 
-    while (headCopy != NULL) {
-        printf("%d ", headCopy->value);
-        headCopy = headCopy->previous;
-    }
-    printf("\n\n");
+    clear(stack, errorCode);
+    free(stack);
 }
 
-int main(void)
-{
-    int errorCode = 0;
-    Unit* head = NULL;
+// Печатает все значения в стеке как целые числа, начиная от головы.
+// Если на вход передан NULL, печатает предупреждение в консоль и меняет код ошибки на 1.
+void printStack(Stack* stack, ErrorCodes* errorCode) {
+    if (stack == NULL) {
+        *errorCode = printStackGotNullPointer;
+        printf("There's no stack\n\n");
+        return;
+    }
 
-    printf("%d\n\n", isEmpty(&head));
+    if (stack->head == NULL) {
+        printf("Stack is empty\n\n");
+        return;
+    }
 
-    printStack(&head);
+    StackElement* headCopy = stack->head;
 
-    push(10, &head, &errorCode);
-
-    printStack(&head);
-
-    printf("%d\n\n", isEmpty(&head));
-
-    return 0;
+    while (headCopy != NULL) {
+        printf("%d\n", headCopy->value);
+        headCopy = headCopy->previous;
+    }
+    printf("\n");
 }
--- customStack/customStack.h
+++ customStack/customStack.h
@@ -1,11 +1,61 @@
 #pragma once
 
-// Все фунции в случае ошибок меняют переменную errorCode с 0 на 1.
+#include <stdbool.h>
 
-// Структура для единицы стека.
-// Содержит два поля: значение и указатель на предыдущую единицу стека.
-typedef struct Unit {
-    int value;
-    struct Unit* previous;
-} Unit;
+// Коды ошибок:
+// 1 - ошибка выделения памяти под новый стек (createStack).
+// 2 - в качестве указателя на голову стека передан NULL (push).
+// 3 - ошибка выделения памяти под новый элемент стека (push).
+// 4 - в качестве указателя на голову стека передан NULL (pop).
+// 5 - в стеке нет элеменетов для pop (pop).
+// 6 - в качестве указателя на голову стека передан NULL (clear).
+// 7 - в качестве указателя на голову стека передан NULL (isEmpty).
+// 8 - в качестве указателя на голову стека передан NULL (deleteStack).
+// 9 - в качестве указателя на голову стека передан NULL (printStack).
+typedef enum ErrorCodes {
+    noErrors,
+    createStackMemoryError,
+    pushGotNullPointer,
+    pushMemoryError,
+    popGotNullPointer,
+    popStackHasNoElemens,
+    clearGotNullPointer,
+    isEmptyGotNullPointer,
+    deleteStackGotNullPointer,
+    printStackGotNullPointer,
+} ErrorCodes;
 
+// Тип переменных для хранения в элементах стека.
+// Если нужно поменять - меняем тип данных в этом месте.
+typedef int Value;
+
+// Абстрактный тип данных "стек".
+typedef struct Stack Stack;
+
+// Создаёт пустой стек.
+// На вход принимает указатель на переменную с кодом ошибки.
+// Возвращает указатель на стек, если всё прошло нормально,в противном случае возвращает NULL и меняет код ошибки на 1.
+Stack* createStack(ErrorCodes* errorCode);
+
+// Добавляет элемент на верхушку стека.
+// На вход принимает добавляемое значение, указатель на стек и указатель на переменную с кодом ошибки.
+void push(Value value, Stack* stack, ErrorCodes* errorCode);
+
+// Удаляет элемент с вершины стека и возвращает значение, которое в нём лежало.
+// На вход принимает указатель на стек и указатель на переменную с кодом ошибки.
+Value pop(Stack* stack, ErrorCodes* errorCode);
+
+// Удаляет все элементы из стека, никуда их не сохраняя.
+// На вход принимает указатель на стек и указатель на переменную с кодом ошибки.
+void clear(Stack* stack, ErrorCodes* errorCode);
+
+// Возвращает true, если стек пуст, и false в противном случае.
+// Если в качестве указателя на стек передан NULL, возвращает false и меняет код ошибки на 1.
+bool isEmpty(Stack* stack, ErrorCodes* errorCode);
+
+// Удалить указанный стек.
+void deleteStack(Stack* stack, ErrorCodes* errorCode);
+
+// Печатает все значения в стеке как целые числа, начиная от головы.
+// Если на вход передан NULL, печатает предупреждение в консоль и меняет код ошибки на 1.
+void printStack(Stack* stack, ErrorCodes* errorCode);
