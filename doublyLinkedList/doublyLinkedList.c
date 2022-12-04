eded18333a9fc44c8d446ea4cd847ecf5d6bb4fc
Tim <g.phh@mail.ru>
2022-12-04 22:18:54 +0300

    First version of list without tests.

--- doublyLinkedList/doublyLinkedList.c
+++ doublyLinkedList/doublyLinkedList.c
@@ -1,11 +1,217 @@
-﻿#include <stdio.h>
+﻿#include "doublyLinkedList.h"
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-bool test(void) {
+// Структура для элементов списка.
+// Поля: значение, указатели на предыдущий и следующий элементы.
+typedef struct ListElement {
+    Value value;
+    struct ListElement* prev;
+    struct ListElement* next;
+} ListElement;
 
+// АТД структура "двусвязный список".
+// Элемент списка - структура ListElement.
+// Поля: указатель на начало списка, указатель на конец списка, количество элементов в списке.
+typedef struct LinkedList {
+    ListElement* head;
+    ListElement* tail;
+    int size;
+} LinkedList;
+
+LinkedList* createLinkedList(ErrorCodes* errorCode) {
+    LinkedList* newList = malloc(sizeof(LinkedList));
+    if (newList == NULL) {
+        *errorCode = createListMemoryError;
+        return NULL;
+    }
+
+    newList->head = NULL;
+    newList->tail = NULL;
+    newList->size = 0;
+
+    return newList;
+}
+
+void pushToHead(Value value, LinkedList* list, ErrorCodes* errorCode) {
+    if (list == NULL) {
+        *errorCode = pushToHeadGotNullPointer;
+        return;
+    }
+
+    ListElement* newElement = malloc(sizeof(ListElement));
+    if (newElement == NULL) {
+        *errorCode = pushToHeadMemoryError;
+        return;
+    }
+
+    newElement->value = value;
+    newElement->prev = NULL;
+    newElement->next = list->head;
+
+    list->head = newElement;
+
+    if (list->size == 0) {
+        list->tail = newElement;
+    }
+
+    list->size += 1;
+}
+
+void pushToTail(Value value, LinkedList* list, ErrorCodes* errorCode) {
+    if (list == NULL) {
+        *errorCode = pushToTailGotNullPointer;
+        return;
+    }
+
+    ListElement* newElement = malloc(sizeof(ListElement));
+    if (newElement == NULL) {
+        *errorCode = pushToTailMemoryError;
+        return;
+    }
+
+    newElement->value = value;
+    newElement->prev = list->tail;
+    newElement->next = NULL;
+
+    list->tail = newElement;
+
+    if (list->size == 0) {
+        list->head = newElement;
+    }
+
+    list->size += 1;
+}
+
+// Вспомогательная функция.
+// Возвращает указатель на n-тый элемент списка.
+ListElement* getNthElement(LinkedList* list, int n) {
+    if (n <= list->size / 2) {
+        ListElement* elementToReturn = list->head;
+
+        for (int i = 1; i < n; ++i) {
+            elementToReturn = elementToReturn->next;
+        }
+
+        return elementToReturn;
+    }
+
+    ListElement* elementToReturn = list->tail;
+
+    for (int i = 0; i < list->size - n; ++i) {
+        elementToReturn = elementToReturn->prev;
+    }
+
+    return elementToReturn;
+}
+
+void insertNthElement(Value value, int n, LinkedList* list, ErrorCodes* errorCode) {
+    if (list == NULL) {
+        *errorCode = insertNthElementGotNullPointer;
+        return;
+    }
+
+    if (n > list->size) {
+        *errorCode = noSuchNumberInList;
+        return;
+    }
+
+    ListElement* currentNthElement = getNthElement(list, n);
+
+    ListElement* newElement = malloc(sizeof(ListElement));
+    if (newElement == NULL) {
+        *errorCode = insertNthElementMemoryError;
+        return;
+    }
+
+    newElement->value = value;
+    newElement->prev = currentNthElement->prev;
+    newElement->next = currentNthElement;
+
+    currentNthElement->prev->next = newElement;
+    currentNthElement->prev = newElement;
+
+    list->size += 1;
+}
+
+Value popFromHead(LinkedList* list, ErrorCodes* errorCode) {
+    if (list == NULL) {
+        *errorCode = popFromHeadGotNullPointer;
+        return NULL;
+    }
+
+    if (list->size == 0) {
+        *errorCode = popFromHeadGotEmptyList;
+        return NULL;
+    }
+
+    Value valueToReturn = list->head->value;
+    ListElement* temporary = list->head->next;
+    free(list->head);
+    list->head = temporary;
+
+    return valueToReturn;
+}
+
+Value popFromTail(LinkedList* list, ErrorCodes* errorCode) {
+    if (list == NULL) {
+        *errorCode = popFromTailGotNullPointer;
+        return;
+    }
+
+    if (list->size == 0) {
+        *errorCode = popFromTailGotEmptyList;
+        return NULL;
+    }
+
+    Value valueToReturn = list->tail->value;
+    ListElement* temporary = list->tail->prev;
+    free(list->tail);
+    list->tail = temporary;
+
+    return valueToReturn;
+}
+
+Value popNthElement(LinkedList* list, int n, ErrorCodes* errorCode) {
+    if (list == NULL) {
+        *errorCode = popNthElementGotNullPointer;
+        return NULL;
+    }
+
+    if (list->size == 0) {
+        *errorCode = popNthElementGotEmptyList;
+        return NULL;
+    }
+
+    ListElement* currentNthElement = getNthElement(list, n);
+
+    Value valueToReturn = currentNthElement->value;
+
+    currentNthElement->prev->next = currentNthElement->next;
+    currentNthElement->next->prev = currentNthElement->prev;
+
+    free(currentNthElement);
+    return valueToReturn;
+}
+
+Value getNthValue(LinkedList* list, int n, ErrorCodes* errorCode) {
+    if (list == NULL) {
+        *errorCode = getNthValueGotNullPointer;
+        return;
+    }
+
+    if (list->size == 0) {
+        *errorCode = getNthValueGotEmptyList;
+        return NULL;
+    }
+
+    return getNthElement(list, n)->value;
 }
 
 int main(void) {
+
 
 
     return 0;
--- doublyLinkedList/doublyLinkedList.h
+++ doublyLinkedList/doublyLinkedList.h
@@ -4,24 +4,55 @@
 // 
 typedef enum ErrorCodes {
     noErrors,
-};
+    createListMemoryError,
+    pushToHeadGotNullPointer,
+    pushToHeadMemoryError,
+    pushToTailGotNullPointer,
+    pushToTailMemoryError,
+    insertNthElementGotNullPointer,
+    noSuchNumberInList,
+    insertNthElementMemoryError,
+    popFromHeadGotNullPointer,
+    popFromHeadGotEmptyList,
+    popFromTailGotNullPointer,
+    popFromTailGotEmptyList,
+    getNthValueGotNullPointer,
+    getNthValueGotEmptyList,
 
+
+} ErrorCodes;
+
+// Тип данных, хранящихся в списке. Если нужно его поменять - меняй здесь.
 typedef int Value;
 
+// АТД структура "двусвязный список".
 typedef struct LinkedList LinkedList;
 
+// Создать список.
+// Возвращает указатель на созданный список.
 LinkedList* createLinkedList(ErrorCodes* errorCode);
 
-int getFirstOrdinalWithSuchValue(Value value, LinkedList* list, ErrorCodes* errorCode);
+// Добавить элемент в начало списка.
+void pushToHead(Value value, LinkedList* list, ErrorCodes* errorCode);
 
-void addToEnd(Value value, LinkedList* list, ErrorCodes* errorCode);
+// Добавить элемент в конец списка.
+void pushToTail(Value value, LinkedList* list, ErrorCodes* errorCode);
 
-void addToBeginning(Value value, LinkedList* list, ErrorCodes* errorCode);
+// Вставить элемент на n-тое место в списке (нумерация от 1).
+void insertNthElement(Value value, int n, LinkedList* list, ErrorCodes* errorCode);
 
-void addAfterValue(Value addedValue, Value previousValue, LinkedList* list, ErrorCodes* errorCode);
+// Получить значение первого элемента в списке и удалить этот элемент.
+// Возвращает значение, которое лежало в этом элементе.
+Value popFromHead(LinkedList* list, ErrorCodes* errorCode);
 
-void addBeforeValue(Value addedValue, Value nextValue, LinkedList* list, ErrorCodes* errorCode);
+// Получить значение последнего элемента в списке и удалить этот элемент.
+// Возвращает значение, которое лежало в этом элементе.
+Value popFromTail(LinkedList* list, ErrorCodes* errorCode);
 
-void addAfterOrdinal(Value addedValue, Value previousOrdinal, LinkedList* list, ErrorCodes* errorCode);
+// Получить значение n-того элемента в списке (нумерация от 1) и удалить этот элемент.
+// Возвращает значение, которое лежало в этом элементе.
+Value popNthElement(LinkedList* list, int n, ErrorCodes* errorCode);
 
-void addBeforeOrdinal(Value addedValue, Value nextOrdinal, LinkedList* list, ErrorCodes* errorCode);
+// Получить значение n-того элемента в списке (нумерация от 1), не удаляя этот элемент.
+// Возвращает значение, которое лежит в этом элементе.
+Value getNthValue(LinkedList* list, int n, ErrorCodes* errorCode);
