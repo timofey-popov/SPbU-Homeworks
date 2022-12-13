58788ce49bdadfbef898e6cbfa6404a06adee14f
Tim <g.phh@mail.ru>
2022-12-13 17:17:26 +0300

    Add file reading and test file for sort.

--- upgradedMostFrequentElement/sort.c
+++ upgradedMostFrequentElement/sort.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
+// Подсчёт количества записей (целые числа через пробел) в файле.
+int countRecords(FILE* file) {
+    int counter = 0;
+
+    while (fscanf_s(file, "%*d") != EOF) {
+        counter += 1;
+    }
+
+    rewind(file);
+    return counter;
+}
+
+// Чтение данных из файла.
+// На вход принимает указатель на файл, указатель на массив и количество записей в файле.
+void fileReading(FILE* file, int* arrayForNumbers, int numberOfRecords) {
+    for (int i = 0; i < numberOfRecords; ++i) {
+        fscanf_s(file, "%d", &arrayForNumbers[i]);
+    }
+
+    rewind(file);
+}
+
+// Поэлементное копирование исходного массива в массив назначения.
+void returnArray(int* arrayFrom, int* arrayTo, int arraysSize) {
+    for (int i = 0; i < arraysSize; ++i) {
+        arrayTo[i] = arrayFrom[i];
+    }
+}
 
 // Функция разделения массива на две части.
 // Слева элементы меньшие либо равные последнему элементу массива, справа - бОльшие последнего.
 // В конце выполнения помещает последний элемент (разделитель) между этими двумя частями массива.
 // Возвращает индекс этого элемента-разделителя.
+// Нумерация от 0 до n - 1;
 int partition(int* arrayToSort, int leftElementNumber, int rightElementNumber) {
     int pivot = arrayToSort[rightElementNumber];
 
@@ -30,9 +61,11 @@ int partition(int* arrayToSort, int leftElementNumber, int rightElementNumber) {
 // Вызывает себя от начала до индекса, возвращаемого функцией разделения, - 1, и от индекса + 1 до конца массива.
 // Сам элемент с индексом, возвращённым функцией разделения, оставляет на месте.
 // Если элементов в массиве меньше 10, функция сортирует их обычными вставками.
-void quickSort(int* arrayToSort, int leftElementNumber, int rightElementNumber) {
+// Нумерация от 0 до n - 1;
+void quickSortExecution(int* arrayToSort, int leftElementNumber, int rightElementNumber) {
+
     if ((rightElementNumber - leftElementNumber + 1) < 10) {
-        for (int i = leftElementNumber + 1; i < rightElementNumber + 1; ++i) {
+        for (int i = leftElementNumber + 1; i <= rightElementNumber; ++i) {
             int k = i;
             while (k > leftElementNumber && arrayToSort[k] < arrayToSort[k - 1]) {
                 int temporary = arrayToSort[k];
@@ -46,7 +79,29 @@ void quickSort(int* arrayToSort, int leftElementNumber, int rightElementNumber) 
 
     else if (leftElementNumber < rightElementNumber) {
         int separator = partition(arrayToSort, leftElementNumber, rightElementNumber);
-        quickSort(arrayToSort, leftElementNumber, separator - 1);
-        quickSort(arrayToSort, separator + 1, rightElementNumber);
+        quickSortExecution(arrayToSort, leftElementNumber, separator - 1);
+        quickSortExecution(arrayToSort, separator + 1, rightElementNumber);
     }
+}
+
+int quickSort(FILE* file, int* arrayForResult) {
+    int numberOfRecords = countRecords(file);
+    if (numberOfRecords == 0) {
+        return 0;
+    }
+
+    int* arrayToSort = calloc(numberOfRecords, sizeof(int));
+    if (arrayToSort == NULL) {
+        return -1;
+    }
+
+    fileReading(file, arrayToSort, numberOfRecords);
+
+    quickSortExecution(arrayToSort, 0, numberOfRecords - 1);
+
+    returnArray(arrayToSort, arrayForResult, numberOfRecords);
+
+    free(arrayToSort);
+
+    return 0;
 }
--- upgradedMostFrequentElement/upgradedMostFrequentElement.c
+++ upgradedMostFrequentElement/upgradedMostFrequentElement.c
@@ -1,11 +1,25 @@
 ﻿#include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "sort.h"
+
+// Подсчёт количества чисел в файле, записанных через пробел.
+int getArraySize(FILE* file) {
+    int counter = 0;
+
+    while (fscanf_s(file, "%*d") != EOF) {
+        counter += 1;
+    }
+
+    rewind(file);
+    return counter;
+}
 
 // Функция, считающая самый частый элемент в массиве.
 // На вход принимает ссылку на массив и его размер, возвращает значение самого частого элемента.
-int mostFrequentElement(int* arrayToOperate, size_t arraySize) {
-    quickSort(arrayToOperate, 0, arraySize - 1);
+int mostFrequentElementExecution(FILE* file, int* arrayToOperate, size_t arraySize) {
+
+    quickSort(file, arrayToOperate);
 
     int counter = 1;
     int maximumCounter = 1;
@@ -31,25 +45,50 @@ int mostFrequentElement(int* arrayToOperate, size_t arraySize) {
 // Функция-тест.
 // Возвращает true, если всё хорошо, и false в противном случае.
 bool test(void) {
-    int testArray1[] = { 1, 2, 3, 4, 4, 5 };
-    int testArray2[] = { 0, 0, 0, 0, 0, 0 };
-    int testArray3[] = { 1, 2, 3, 4, 5, 6 };
+    FILE* testFile = fopen("testFileForMostFrequentElement.txt", "r");
+    if (testFile == NULL) {
+        return false;
+    }
 
-    return (mostFrequentElement(testArray1, 6) == 4 && mostFrequentElement(testArray2, 6) == 0 && mostFrequentElement(testArray3, 6) == 1);
+    int* testArray = calloc(getArraySize(testFile), sizeof(int));
+    if (testArray == NULL) {
+        return false;
+    }
+
+    return (mostFrequentElementExecution(testFile, testArray, getArraySize(testFile)) == 7);
 }
 
 int main(void) {
     if (!test()) {
         printf("Test failed.\n");
         return -1;
-    } else {
+    }
+    else {
         printf("*test passed*\n\n");
     }
 
-    int arrayToOperate[] = { 1, 5, 5, 6, 2, 3, 4, 7, 5, 1, 2, 3, 1, 1 };
-    size_t sizeOfArray = sizeof(arrayToOperate) / sizeof(int);
+    FILE* fileToSort = fopen("fileToSort.txt", "a+");
+    if (fileToSort == NULL) {
+        return -1;
+    }
 
-    printf("Most frequent element = %d\n", mostFrequentElement(arrayToOperate, sizeOfArray));
+    int counterOfRecords = getArraySize(fileToSort);
+    if (counterOfRecords == 0) {
+        fclose(fileToSort);
+        printf("No elements in file\n");
+        return 0;
+    }
+
+    int* arrayToOperate = calloc(counterOfRecords, sizeof(int));
+    if (arrayToOperate == NULL) {
+        fclose(fileToSort);
+        return -1;
+    }
+
+    printf("Most frequent element = %d\n", mostFrequentElementExecution(fileToSort, arrayToOperate, counterOfRecords));
+
+    free(arrayToOperate);
+    fclose(fileToSort);
 
     return 0;
 }
