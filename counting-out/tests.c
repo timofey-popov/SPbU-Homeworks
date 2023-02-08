d843898a8c256b038e3cbd1340078969725f35fe
Tim <g.phh@mail.ru>
2023-02-08 22:10:05 +0300

    Reveal functions for interacting with ADT "Circular list".

--- counting-out/tests.c
+++ counting-out/tests.c
@@ -1,7 +1,104 @@
 #include "tests.h"
+#include "circularList.h"
+
+#include <stdlib.h>
+
+// Тест для функций создания и удаления списка.
+bool testForCreateAndDeleteList(void) {
+    ListErrors testErrorCode = noErrorsInList;
+
+    CircularList* testList = createList(&testErrorCode);
+    if (testErrorCode || (testList == NULL)) {
+        return false;
+    }
+
+    deleteList(testList, &testErrorCode);
+
+    return !testErrorCode;
+}
+
+// Тест для разных сценариев добавления и удаления элементов из списка.
+bool testForAddAndDeleteValue(void) {
+    ListErrors testErrorCode = noErrorsInList;
+
+    CircularList* testList = createList(&testErrorCode);
+    if (testErrorCode || (testList == NULL)) {
+        return false;
+    }
+
+    deleteFromList(100, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    addToList(1, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    deleteFromList(50, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    deleteFromList(1, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    addToList(-6, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    addToList(15, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    deleteFromList(3, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    deleteFromList(15, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    addToList(20, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    deleteFromList(-6, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    deleteFromList(20, testList, &testErrorCode);
+    if (testErrorCode) {
+        deleteList(testList, &testErrorCode);
+        return false;
+    }
+
+    deleteList(testList, &testErrorCode);
+
+    return !testErrorCode;
+}
 
 bool testForCalculating(void) {
-    CountingOutErrors testErrorCode = noErrors;
+    ListErrors testErrorCode = noErrorsInList;
 
     // Легко проверить вручную.
     int test1 = calculateSuitablePosition(5, 2, &testErrorCode);
@@ -13,5 +110,9 @@ bool testForCalculating(void) {
     int test3 = calculateSuitablePosition(2, 120, &testErrorCode);
     int test4 = calculateSuitablePosition(2, 159, &testErrorCode);
 
-    return (test1 == 3) && (test2 == 1) && (test3 == 1) && (test4 == 2);
+    // Всегда должен возвращать последнюю позицию.
+    int test5 = calculateSuitablePosition(120, 1, &testErrorCode);
+    int test6 = calculateSuitablePosition(3, 1, &testErrorCode);
+
+    return (test1 == 3) && (test2 == 1) && (test3 == 1) && (test4 == 2) && (test5 == 120) && (test6 == 3);
 }
