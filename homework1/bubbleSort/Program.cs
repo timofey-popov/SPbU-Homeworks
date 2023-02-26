82c8d0b60f909a8c50e0a2e0c9fb566915163a00
Tim <g.phh@mail.ru>
2023-02-26 16:57:14 +0300

    Initial commit (working version).

---
+++ homework1/bubbleSort/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SortPrograms
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] inputArray = { 5, 4, 3, 2, 1 };
+
+            BubbleSort.BubbleSortExecution(inputArray);
+
+            string resultString = string.Join(" ", inputArray);
+            Console.WriteLine(resultString);
+        }
+    }
+
+    class BubbleSort
+    {
+        public static void BubbleSortExecution(int[] arrayForSort)
+        {
+            for (int i = 0; i < arrayForSort.Length - 1; i++)
+            {
+                for (int j = 0; j < arrayForSort.Length - i - 1; j++)
+                {
+                    (arrayForSort[j + 1], arrayForSort[j])=(arrayForSort[j], arrayForSort[j + 1]);
+                }
+            }
+        }
+    }
+}
