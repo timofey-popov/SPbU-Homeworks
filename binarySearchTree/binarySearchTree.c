29497aa5fba02025bc6d5b71d676ec919b96e0fe
Tim <g.phh@mail.ru>
2023-04-11 18:25:34 +0300

    Initial commit with .h done.

--- binarySearchTree/binarySearchTree.c
+++ binarySearchTree/binarySearchTree.c
@@ -1,1 +1,29 @@
-﻿
+﻿#include "binarySearchTree.h"
+
+#include <stdlib.h>
+
+// Структура для элементов словаря.
+typedef struct DictionaryElement {
+    int key;
+    Value value;
+    DictionaryElement* leftChild;
+    DictionaryElement* rightChild;
+} DictionaryElement;
+
+// Реализация АТД "Словарь".
+struct Dictionary {
+    DictionaryElement* root;
+};
+
+Dictionary* createDictionary(ErrorCodes* errorCode) {
+    Dictionary* newDictionary = malloc(sizeof(Dictionary));
+    if (newDictionary == NULL) {
+        *errorCode = memoryAllocationError;
+    }
+
+    return newDictionary;
+}
+
+void deleteDictionary(Dictionary* dictionaryToRemove, ErrorCodes* errorCode) {
+
+}
--- binarySearchTree/binarySearchTree.h
+++ binarySearchTree/binarySearchTree.h
@@ -1,12 +1,40 @@
 #pragma once
 
+#include <stdbool.h>
+
+// Коды ошибок.
 typedef enum ErrorCodes {
     noErrors,
     memoryAllocationError,
-    nullPointerReceived,
-    treeIsEmpty,
-
+    gotNullPointer,
+    treeIsEmpty
 } ErrorCodes;
 
+// АТД "Словарь".
+// Позволяет организовать хранение данных и поиск значений по ключу.
+typedef struct Dictionary Dictionary;
+
+// Тип данных для хранимых в словаре значений.
 typedef int Value;
 
+// Создать словарь. Возвращает указатель на созданный словарь или NULL, если произошла ошибка.
+Dictionary* createDictionary(ErrorCodes* errorCode);
+
+// Добавить значение в словарь с заданным ключом.
+// На вход принимает значение, ключ, словарь и переменную с кодом ошибки.
+// Если такой ключ уже был в словаре, значение заменяется на новое.
+void addValue(Value value, int key, Dictionary* dictionary, ErrorCodes* errorCode);
+
+// Получить значение по ключу. На вход принимает ключ, словарь и переменную с кодом ошибки.
+// Если такого ключа в словаре нет, возвращает NULL и не изменяет код ошибки.
+Value getValueByKey(int key, Dictionary* dictionary, ErrorCodes* errorCode);
+
+// Вернёт true, если такой ключ есть в словаре, и false в обратном случае.
+bool isSuchKey(int key, Dictionary* dictionary, ErrorCodes* errorCode);
+
+// Удалить ключ и связанное с ним значение из словаря.
+// Если такого ключа в словаре нет, ничего не произойдёт и код ошибки не поменяется.
+void deleteKeyAndItsValue(int key, Dictionary* dictionary, ErrorCodes* errorCode);
+
+// Удалить словарь. Возвращает true, если всё прошло успешно, и false в противном случае.
+void deleteDictionary(Dictionary* dictionaryToRemove, ErrorCodes* errorCode);
